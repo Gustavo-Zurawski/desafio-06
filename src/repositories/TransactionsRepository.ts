bc319e5ea094731c8dc489f0dbe63dd7d36cb886
Gustavo-Zurawski <67868250+Gustavo-Zurawski@users.noreply.github.com>
2020-08-29 14:35:41 -0300

    Update TransactionsRepository.ts

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const { transactions } = this;
+
+    return transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const incomes = this.transactions.reduce(
+      (acumulador: number, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          return acumulador + transaction.value;
+        }
+        return acumulador;
+      },
+      0,
+    );
+
+    const outcomes = this.transactions.reduce(
+      (acumulador: number, transaction: Transaction) => {
+        if (transaction.type === 'outcome') {
+          return acumulador + transaction.value;
+        }
+        return acumulador;
+      },
+      0,
+    );
+
+    const total = incomes - outcomes;
+
+    return {
+      income: incomes,
+      outcome: outcomes,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
